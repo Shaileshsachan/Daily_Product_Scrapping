9e3af4462b92d2f0464f5f53b970bd7c9d547afb
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-02-04 16:37:09 +0530

    'just_change_link'

---
+++ flipkart_scrape.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
+products = []
+prices = []
+ratings = []
+discounts = []
+# driver.get("https://www.flipkart.com/search?q=iphone+11&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off&as-pos=1&as-type=HISTORY&as-backfill=on")
+# driver.get("https://www.flipkart.com/search?q=laptops&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off")
+content = driver.page_source
+
+soup = BeautifulSoup(content, features='lxml')
+for a in soup.find_all('div', {'class': '_2kHMtA'}):
+    name = a.find(class_ = '_4rR01T')
+    price = (a.find(class_ ='_30jeq3 _1_WHN1')).get_text()[1:]
+    rating = a.find(class_ = '_3LWZlK')
+    discount = (a.find(class_ = '_3Ay6Sb'))
+    if discount == None:
+        pass
+    else:
+        discount = discount.get_text()
+    products.append(name.text)
+    prices.append(price)
+    ratings.append(rating.text)
+    discounts.append(discount)
+
+df = pd.DataFrame({'ProductName': products, 'Price':prices, 'Rating':ratings, 'Discount': discounts})
+df.to_csv('products1.csv', index=False, encoding='utf-8')
